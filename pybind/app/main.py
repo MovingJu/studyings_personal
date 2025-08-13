4de135ba701b6eb280186eb7c7772dc6ad58b64c
MovingJu <baramishe@gmail.com>
2025-08-14 00:06:04 +0900

    .

--- pybind/app/main.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('../build')
-
-import mymodule
-
-print(mymodule.square(5))
--- pybind/src/mymodule.cpp
+++
@@ -1,9 +0,0 @@
-#include <pybind11/pybind11.h>
-
-int square(int a){
-    return a * a;
-}
-
-PYBIND11_MODULE(mymodule, m){
-    m.def("square", &square, "squarer");
-}
