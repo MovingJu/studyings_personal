bf2e44358b4cddd84b9151b6acccd218848dbbae
MovingJu <baramishe@gmail.com>
2025-08-19 09:34:33 +0900

    .

---
+++ typescript/cdap/index.js
@@ -0,0 +1,24 @@
+function get_last_subject(teacher) {
+    if (typeof teacher.subject === "string") {
+        return teacher.subject;
+    }
+    if (Array.isArray(teacher.subject)) {
+        return teacher.subject[teacher.subject.length - 1];
+    }
+    else {
+        return "None";
+    }
+}
+function main() {
+    console.log(get_last_subject({
+        subject: ["science", "math"]
+    }));
+    console.log(get_last_subject({
+        subject: "korean"
+    }));
+    console.log(get_last_subject({
+        hello: "hi"
+    }));
+    return;
+}
+main();
